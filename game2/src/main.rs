58dd16ca4fd46905326e7b68fb8145e28a3a896e
Evil_Zub <evilzub@posteo.de>
2023-10-26 20:24:41 +0200

    make it move

--- game2/src/main.rs
+++ game2/src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use model::gameobjects::GameObject;
 use sdl2::{event::Event, keyboard::Keycode};
 
@@ -12,9 +14,24 @@ fn main() {
     let mut running = true;
     let mut event_queue = playfield.sdl_context.event_pump().unwrap();
 
-    let mut player = GameObject::new(playfield.screen_width / 2, playfield.screen_height / 2 );
+    let mut player = GameObject::new(
+        playfield.screen_width,
+        playfield.screen_height,
+        (playfield.screen_width / 2) as i16,
+        (playfield.screen_height / 2) as i16);
 
     while running {
+
+        // FIXME: Use states like this:
+        // const Uint8* keystates = SDL_GetKeyboardState(NULL);
+
+        // ...
+
+        // if(keystates[SDL_SCANCODE_LEFT])
+        //     player.moving = player.left;
+        // else if(keystates[SDL_SCANCODE_RIGHT])
+        //     player.moving = player.right;
+
         for event in event_queue.poll_iter() {
             match event {
                 Event::Quit {..} => {
@@ -24,14 +41,26 @@ fn main() {
                     match keycode {
                         Keycode::Left => player.rotate_counterclockwise(),
                         Keycode::Right => player.rotate_clockwise(),
+                        Keycode::Up => player.increase_thrust(),
                         _ => {}
                     }
-                    println!("Rotation angle: {}", player.angle_deg);
+                },
+                Event::KeyUp { keycode: Some(keycode), ..} => {
+                    match keycode {
+                        Keycode::Up => {
+                            player.decrease_thrust();
+                        },
+                        _ => {}
+                    }
                 },
                 _ => {}
             }
         }
 
+        player.update();
         playfield.render(&player);
+
+        // don't know how this works exactly, but SDL2 docs say this is the way to limit to 60 fps
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
--- game2/src/model/gameobjects.rs
+++ game2/src/model/gameobjects.rs
@@ -1,7 +1,9 @@
 use sdl2::{render::Canvas, video::Window, pixels::Color};
 use sdl2::gfx::primitives::DrawRenderer;
 
-const TURNING_SPEED: u32 = 8;
+const TURNING_SPEED: u32 = 12;
+const THRUST: f32 = 0.2;
+const MAX_SPEED: f32 = 8.;
 
 pub struct Vector2 {
     pub x: f32,
@@ -9,6 +11,10 @@ pub struct Vector2 {
 }
 
 impl Vector2 {
+    pub fn new() -> Vector2 {
+        Vector2 { x: 0., y: 0. }
+    }
+
     pub fn rotate(&self, angle_deg: i16) -> Vector2 {
         let angle = angle_deg as f32;
         let angle_rad = angle.to_radians();
@@ -18,26 +24,64 @@ impl Vector2 {
         }
     }
 
-    pub fn new_thrust_vector() -> Vector2 {
-        // Thrustvector points upward in neutral and is rotated later
-        Vector2 { x: 0., y: -1. }
+    pub fn magnitude(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+
+    pub fn add(&self, other: &Vector2) -> Vector2 {
+        Vector2 { x: self.x + other.x, y: self.y + other.y }
+    }
+
+    pub fn multiply(&self, num: f32) -> Vector2 {
+        if num.is_nan() {
+            Vector2 { x: self.x, y: self.y }
+        } else {
+            Vector2 { x: self.x * num, y: self.y * num }
+        }
     }
 }
 
 pub struct GameObject {
     // choosing u32 for no particular reason
-    pub x: u32,
-    pub y: u32,
+
+    // need screen dimension to calculate wrap-around
+    screen_width: u32,
+    screen_height: u32,
+
+    pub x: i16,
+    pub y: i16,
 
     pub angle_deg: i16,
+
+    thrust: f32,
     thrust_vector: Vector2,
+    velocity_vector: Vector2,
 }
 
 impl GameObject {
 
+    // if value exceeds boundary, wrap it to the "left" side of the range and the other way round
+    fn wrap_around(n: i16, boundary: i16) -> i16 {
+        if n > boundary {
+            return n - boundary
+        } else if n < 0 {
+            return boundary + n
+        }
+        n
+    }
+
     // static new method as "constructor"
-    pub fn new(x: u32, y: u32) -> GameObject {
-        GameObject { x: x, y: y, angle_deg: 0, thrust_vector: Vector2 { x: 0.0, y: -1.0 } }
+    pub fn new(screen_width: u32, screen_height: u32, x: i16, y: i16) -> GameObject {
+        GameObject {
+            screen_width: screen_width,
+            screen_height: screen_height,
+            x: x,
+            y: y,
+            angle_deg: 0,
+            thrust: 0.,
+            thrust_vector: Vector2::new(),
+            velocity_vector: Vector2::new()
+        }
     }
 
     pub fn rotate_clockwise(&mut self) {
@@ -45,7 +89,6 @@ impl GameObject {
         if self.angle_deg >= 360 {
             self.angle_deg = self.angle_deg - 360;
         }
-        self.thrust_vector = Vector2::new_thrust_vector().rotate(self.angle_deg);
     }
 
     pub fn rotate_counterclockwise(&mut self) {
@@ -53,7 +96,34 @@ impl GameObject {
         if self.angle_deg < 0 {
             self.angle_deg = 360 + self.angle_deg;
         }
-        self.thrust_vector = Vector2::new_thrust_vector().rotate(self.angle_deg);
+    }
+
+    pub fn increase_thrust(&mut self) {
+        self.thrust = THRUST;
+    }
+
+    pub fn decrease_thrust(&mut self) {
+        self.thrust = 0.;
+    }
+
+    pub fn update(&mut self) {
+        self.thrust_vector = Vector2{x: 0., y: self.thrust as f32}.rotate(self.angle_deg);
+
+        self.velocity_vector = self.velocity_vector.add(&self.thrust_vector);
+
+        // observe the speed limit
+        let speed = self.velocity_vector.magnitude();
+        if speed > MAX_SPEED {
+            let limiter = MAX_SPEED / speed;
+            println!("[{},{}] Limiter: {}, Speed: {}", self.velocity_vector.x, self.velocity_vector.y, limiter, speed);
+            self.velocity_vector = self.velocity_vector.multiply(limiter);
+        }
+
+        self.x += self.velocity_vector.x.round() as i16;
+        self.y += self.velocity_vector.y.round() as i16;
+
+        self.x = GameObject::wrap_around(self.x, self.screen_width as i16);
+        self.y = GameObject::wrap_around(self.y, self.screen_height as i16);
     }
 
     pub fn render(&self, canvas: &mut Canvas<Window>) {
@@ -65,12 +135,14 @@ impl GameObject {
             Color::YELLOW).unwrap();
 
         // paint thrust vector in green
+        let indicator = Vector2 { x: 0., y: 1.}.rotate(self.angle_deg);
+
         canvas.line(
             self.x as i16,
             self.y as i16,
             // multiply with scaling factor to make it visible
-            self.x as i16 + (self.thrust_vector.x * 20.) as i16,
-            self.y as i16 + (self.thrust_vector.y * 20.) as i16,
+            self.x as i16 + (indicator.x * 20.) as i16,
+            self.y as i16 + (indicator.y * 20.) as i16,
             Color::GREEN
         ).unwrap();
     }
